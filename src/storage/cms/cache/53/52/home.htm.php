5e2b848fa8bb1343645f4c5e9e0fe2ed5b378b3f
Hiroyuki Suzuki <hiroyuki.suzuki@smartscan.co.jp>
2021-03-29 19:50:14 +0900

    CSMのdefaultファイル追加

---
+++ src/storage/cms/cache/53/52/home.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms6061afe4c8d1e329973686_b770e8c8db4864baea51da57c7b2f914Class extends Cms\Classes\PageCode
+{
+
+}
---
+++ src/storage/cms/cache/eb/e0/default.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms6061afe4c6fd2058319677_c8e45814b4ff35ae0af9386a4b1ed235Class extends Cms\Classes\LayoutCode
+{
+
+}
